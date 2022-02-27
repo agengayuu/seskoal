1a6dfb72ddca0cbcbe15ecd192c73fcca0a4c491
Dhiya'Ulhaq Putri Kinanty <70521610+putriulhaq@users.noreply.github.com>
2022-02-27 23:26:10 +0700

    folder and view main_menu

---
+++ application/controllers/mahasiswa.php
@@ -0,0 +1,28 @@
+<?php
+
+if(!defined('BASEPATH'))
+exit('No direct script access allowed');
+
+class Mahasiswa extends CI_Controller
+{
+
+public function index(){
+
+}
+
+public function tambah(){
+
+}
+
+public function edit(){
+
+}
+public function simpan(){
+
+}
+public function hapus(){
+    
+}
+
+}
+?>
--- application/controllers/main_menu.php
+++ application/controllers/main_menu.php
@@ -5,11 +5,11 @@ exit('No direct script access allowed');
 
 class Main_menu extends CI_Controller
 {
-    public function indec(){
+    public function index(){
 
     $data['user'] = $this->db->get_where('user', ['username'=> 
     $this->session->userdata('username')])->row_array();
-    echo 'selamat datang'  . $data['user']['username'];
+    $this->load->view('main_menu/admin');
 
 
     }
