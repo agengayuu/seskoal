bfede8f4463378207d32f3c07748976d4080050c
agengayuu <ayuageng@apps.ipb.ac.id>
2022-03-07 10:54:14 +0700

    Fix Table Dosen Done

--- application/views/dosen/index.php
+++ application/views/dosen/index.php
@@ -15,37 +15,38 @@
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Daftar Dosen</h6>
         </div>
+
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-borderes" id="dataTable" width="100%" cellspacing="0">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
-                        <tr style="text-align: center;">
+                        <tr>
                             <th>No</th>
                             <th>NIP</th>
                             <th>Nama</th>
                             <th>Email</th>
                             <th>Aksi</th>
                         </tr>
-                    
+                    </thead>
+                    <tbody>
                         <?php
                         $no =1;
                         foreach ($dosen as $d) : ?>
-                    
-                        <tr style="text-align: center;">
+
+                        <tr>
                             <td width="20px"><?php echo $no++ ?></td>
                             <td><?= $d->nip ?></td>
                             <td><?= $d->nama ?></td>
                             <td><?= $d->email ?></td>
-                           
-                            <center>
-                            <td width="15px"> <?php echo anchor('dosen/adminedit/'.$d->id_dosen, '<div class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></div>' ) ?></td>
-                            <td width="15px"> <?php echo anchor('dosen/adminhapus/'.$d->id_dosen, '<div class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></div>' ) ?></td>
-                            </center>
+                            
+                            <td> <?php echo anchor('dosen/adminedit/'.$d->id_dosen, '<div class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></div>' ) ?>
+                            <?php echo anchor('dosen/adminhapus/'.$d->id_dosen, '<div class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></div>' ) ?></td>
+                            
                         </tr>
-                            <?php
-                            endforeach
-                            ?>
-                    </thead>
+                        <?php
+                        endforeach
+                        ?>
+                    </tbody>
                 </table>
             </div>
         </div>
