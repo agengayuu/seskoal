c5f3735e6ae40aef0e3f3159fda8f1101063ecbd
agengayuu <ayuageng@apps.ipb.ac.id>
2022-03-07 10:13:26 +0700

    Fix Table Data Mahasiswa Per Diklat Done

--- application/views/mahasiswa_d/mahasiswa_d.php
+++ application/views/mahasiswa_d/mahasiswa_d.php
@@ -15,33 +15,39 @@
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Data Mahasiswa per Diklat</h6>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-borderes" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr style="text-align: center;">
-                            <th>No</th>
-                            <th>Diklat</th>
-                            <th>Aksi</th>
-                        </tr>
-                        <?php
-                        $no =1;
-                        foreach ($mahasiswa_d as $md) : ?>
 
-                        <tr  style="text-align: center;">
-                            <td width="20px"><?php echo $no++ ?></td>
-                            <td><?= $md->nama_diklat ?>
-                            
-                            <td width="180px"> <?php echo anchor('mahasiswa_d/adminedit/'.$md->id_diklat, '<div class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> Lihat Data</div>' ) ?></td>
-                            </td>
-                        </tr>
-                            <?php
-                            endforeach
-                            ?>
-                        
-                    </thead>
-                </table>
+        <div class="card-body">
+            <div class="table-responsive">                
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Diklat</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $no =1;
+                                foreach ($mahasiswa_d as $md) : ?>
+                                    <tr>
+                                        <td width="20px"><?php echo $no++ ?></td>
+                                        <td><?= $md->nama_diklat ?>
+                                        
+                                        <td width="180px"> <?php echo anchor('mahasiswa_d/adminedit/'.$md->id_diklat, '<div class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> Lihat Data</div>' ) ?></td>
+                                        </td>
+                                    </tr>
+                                        <?php
+                                    endforeach
+                                    ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
     </div>
 </div>
