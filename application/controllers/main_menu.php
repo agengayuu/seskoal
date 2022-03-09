9d2f293efd32d6a39d3851cdc0308789d07a2e29
Dhiya'Ulhaq Putri Kinanty <70521610+putriulhaq@users.noreply.github.com>
2022-03-09 16:30:38 +0700

    pengumuman done

--- application/controllers/main_menu.php
+++ application/controllers/main_menu.php
@@ -21,7 +21,7 @@ class Main_menu extends CI_Controller {
         $data['user'] = $this->db->get_where('user', ['username'=> 
         $this->session->userdata('username')])->row_array();
         
-        $this->load->view('templates_dosen/header'); 
+        $this->load->view('templates_dosen/header',$data); 
         $this->load->view('templates_dosen/sidebar_admin',$data); 
         $this->load->view('main_menu/admin',$data); 
         $this->load->view('templates_dosen/footer',$data); 
@@ -32,22 +32,28 @@ class Main_menu extends CI_Controller {
         $data['user'] = $this->db->get_where('user', ['username'=> 
         $this->session->userdata('username')])->row_array();
         
-        $this->load->view('templates_dosen/header'); 
+        $this->load->view('templates_dosen/header',$data); 
         $this->load->view('templates_dosen/sidebar_admin',$data); 
         $this->load->view('main_menu/dosen',$data); 
         $this->load->view('templates_dosen/footer',$data); 
     }
 
     public function siswa() {
-
         $data['user'] = $this->db->get_where('user', ['username'=> 
         $this->session->userdata('username')])->row_array();
-        
-        $this->load->view('templates_dosen/header'); 
+        $this->load->view('templates_dosen/header',$data); 
         $this->load->view('templates_dosen/sidebar_admin',$data); 
+
+        $query = $this->db->query("select * from tbl_pengumuman")->result();
+        $last = $this->db->order_by('id_pengumuman', 'desc')
+        ->limit(1)
+        ->get('tbl_pengumuman')
+        ->result();
+        //print_r($last);die;
+        $data['pengumuman'] = $last;
+
+        $idterakhir = 
         $this->load->view('main_menu/siswa',$data); 
         $this->load->view('templates_dosen/footer',$data); 
     }
 }
-
-?>
--- application/views/main_menu/siswa.php
+++ application/views/main_menu/siswa.php
@@ -1,169 +1,173 @@
-<div class="container-fluid">
-    <div class="alert alert-success" role="alert">
-        Dashboard
-    </div>
+     <!-- Modal
+     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+         <div class="modal-dialog modal-lg" role="document">
+             <div class="modal-content">
+                 <div class="modal-header">
+                     <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-cogs"></i> Control Panel</h5>
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                         <span aria-hidden="true">&times;</span>
+                     </button>
+                 </div>
+                 <div class="modal-body">
+                     <div class="row">
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">MAHASISWA</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
 
-    <div class="alert alert-success" role="alert">
-        Teks Pokoknya
-        <hr>
-        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
-            <i class="fas fa-cogs"></i> Control Panel
-        </button>
-    </div>
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">MAHASISWA</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-cogs"></i> Control Panel</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">MAHASISWA</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
 
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">MAHASISWA</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
+                     </div>
+                     <hr>
 
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
+                     <div class="row">
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">INPUT NILAI</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
 
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
-                </div><hr>
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">JADWAL</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
 
-                <div class="row">
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">INPUT NILAI</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">RUANGAN</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
 
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">JADWAL</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
+                         <div class="col-md-3 text-info text-center">
+                             <a href="<?php echo base_url() ?>">
+                                 <p class="nav-link small text-info">MAHASISWA</p>
+                             </a>
+                             <i class="fas fa-3x fa-user-graduate"></i>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="modal-footer">
+                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                 </div>
+             </div>
+         </div>
+     </div> -->
 
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">RUANGAN</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
+     <h3>
+         <center>Selamat Datang Mahasiswa di Aplikasi Akademik SESKOAL</center>
+     </h3>
+    
 
-                    <div class="col-md-3 text-info text-center">
-                    <a href="<?php echo base_url() ?>"><p class="nav-link small text-info">MAHASISWA</p></a>
-                        <i class="fas fa-3x fa-user-graduate"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-            </div>
-            </div>
-        </div>
-        </div>
-
-    <h3>
-        <center>Selamat Datang Mahasiswa di Aplikasi Akademik SESKOAL</center>
-    </h3>
-</div>
-
-<!-- Informasi -->
-
-
-<section class="content">
-            <div class="container-fluid">
-                <div class="card mb-4 py-0 border-left-primary">
-                    <div class="card-body">
-                        <h3>Beranda</h3>
-                        Informasi Umum Akademik dan Kemahasiswaan
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-9">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-sm-9">
-                                        <h5 class="card-title">Informasi/Pengumuman</h5>
-                                    </div>
-                                    <div class="col-3">
-                                        <a href="<?php echo base_url('pengumuman_view') ?>" class="btn btn-secondary">Lihat Semua</a>
-                                    </div>
-                                </div>
-                                <label>38 Januari 2090</label>
-                                <h5 class="card-title">Special title treatment</h5>
-                                <p class="card-text">With supporting text below as a natural lead-in to additional content.
-                                                    With supporting text below as a natural lead-in to additional content.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Jadwal</h5>
-                                <p class="card-text">92 Januari 2090.</p>
-                                <div class="card mb-2">
-                                    <div class="card-body">
-                                        <div class="tw-mt-5 tw-border tw-border-gray-200 tw-rounded-xl tw-py-3 tw-px-4">
-                                            <div>
-                                                <h6 class="font-14px tw-font-bold tw-truncate">Laporan Akhir</h6>
-                                            </div>
-                                            <div>
-                                                <h6 class="tw-text-base tw-text-sp-muted-text mb-3">
-                                                    <span class="font-14px tw-inline-block tw-mr-3"> INF399 | P<span>A2</span> </span>
-                                                    <span class="font-14px tw-inline-block tw-text-xs tw-px-4 tw-py-1 tw-bg-sp-muted-text tw-text-white tw-rounded-xl">
-                                                        P
-                                                    </span>
-                                                </h6>
-                                            </div>
-                                            <div>
-                                                <div class="tw-flex tw-items-center tw-mb-2">
-                                                    <h6 class="font-14px tw-text-sp-muted-text">Diklat</h6>
-                                                </div>
-                                                <div class="tw-flex tw-items-center tw-mb-2">
-                                                    <h6 class="font-14px tw-text-sp-muted-text">Selasa</h6>
-                                                </div>
-                                                <div class="tw-flex tw-items-center tw-mb-2">
-                                                    <div class="bg-icon_oclock tw-mr-2"></div>
-                                                    <h6 class="font-14px tw-text-sp-muted-text">
-                                                        8:00 - 16:30 
-                                                    </h6>
-                                                </div>
-                                                <div class="tw-flex tw-items-center">
-                                                        <h6 class="font-14px tw-text-sp-muted-text">Ruangan</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <center>
-                                    <a href="#" class="btn btn-secondary">Lihat Semua Jadwal</a>
-                                </center>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+     <!-- Informasi -->
 
 
-    <!-- end informasi -->
+     <section class="content">
+         <div class="container-fluid">
+             <div class="card mb-4 py-0 border-left-primary">
+                 <div class="card-body">
+                     <h3>Beranda</h3>
+                     Informasi Umum Akademik dan Kemahasiswaan
+                 </div>
+             </div>
+             <div class="row">
+                 <div class="col-sm-9">
+                     <div class="card">
+                         <div class="card-body">
+                             <div class="row">
+                                 <div class="col-sm-9">
+                                     <h5 class="card-title">Informasi/Pengumuman</h5>
+                                 </div>
+                                 <div class="col-3">
+                                     <a href="<?php echo base_url('pengumuman_view') ?>" class="btn btn-secondary">Lihat Semua</a>
+                                 </div>
+                             </div>
+                             <?php foreach($pengumuman as $peng) { ?>
+                             <label><?= $peng-> tgl_pembuatan; ?></label>
+                             <h5 class="card-title"><?= $peng->judul_pengumuman; ?></h5>
+                             <p class="card-text"><?= $peng->isi_pengumuman?>
+                             </p>
+                             <?php } ?>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-sm-3">
+                     <div class="card">
+                         <div class="card-body">
+                             <h5 class="card-title">Jadwal</h5>
+                             <p class="card-text">92 Januari 2090.</p>
+                             <div class="card mb-2">
+                                 <div class="card-body">
+                                     <div class="tw-mt-5 tw-border tw-border-gray-200 tw-rounded-xl tw-py-3 tw-px-4">
+                                         <div>
+                                             <h6 class="font-14px tw-font-bold tw-truncate">Laporan Akhir</h6>
+                                         </div>
+                                         <div>
+                                             <h6 class="tw-text-base tw-text-sp-muted-text mb-3">
+                                                 <span class="font-14px tw-inline-block tw-mr-3"> INF399 | P<span>A2</span> </span>
+                                                 <span class="font-14px tw-inline-block tw-text-xs tw-px-4 tw-py-1 tw-bg-sp-muted-text tw-text-white tw-rounded-xl">
+                                                     P
+                                                 </span>
+                                             </h6>
+                                         </div>
+                                         <div>
+                                             <div class="tw-flex tw-items-center tw-mb-2">
+                                                 <h6 class="font-14px tw-text-sp-muted-text">Diklat</h6>
+                                             </div>
+                                             <div class="tw-flex tw-items-center tw-mb-2">
+                                                 <h6 class="font-14px tw-text-sp-muted-text">Selasa</h6>
+                                             </div>
+                                             <div class="tw-flex tw-items-center tw-mb-2">
+                                                 <div class="bg-icon_oclock tw-mr-2"></div>
+                                                 <h6 class="font-14px tw-text-sp-muted-text">
+                                                     8:00 - 16:30
+                                                 </h6>
+                                             </div>
+                                             <div class="tw-flex tw-items-center">
+                                                 <h6 class="font-14px tw-text-sp-muted-text">Ruangan</h6>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+                             <center>
+                                 <a href="#" class="btn btn-secondary">Lihat Semua Jadwal</a>
+                             </center>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
 
 
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
+     <!-- end informasi -->
 
+
+     <!-- Scroll to Top Button-->
+     <a class="scroll-to-top rounded" href="#page-top">
+         <i class="fas fa-angle-up"></i>
+     </a>
