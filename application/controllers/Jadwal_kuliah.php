417b209ff2ea587bb9615afbbcfdd3743e136ddf
Dhiya'Ulhaq Putri Kinanty <70521610+putriulhaq@users.noreply.github.com>
2022-03-10 13:56:17 +0700

    unfinished jadwal_kuliah
    edit dan update belum

---
+++ application/controllers/Jadwal_kuliah.php
@@ -0,0 +1,98 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Jadwal_kuliah extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('m_jadwal');
+        $this->load->library('session');
+        //session_start();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Jadwal Kuliah';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('templates_dosen/header', $data);
+        $this->load->view('templates_dosen/sidebar_admin', $data);
+        
+
+        $data['jadwal'] = $this->m_jadwal->getdata();
+        $this->load->view('jadwal_kuliah/index',$data);
+        $this->load->view('templates_dosen/footer');
+    }
+
+    public function tambah()
+    {
+        $data['title'] = 'Jadwal Kuliah';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('templates_dosen/header', $data);
+        $this->load->view('templates_dosen/sidebar_admin', $data);
+
+        $diklatnya =  $this->db->query("select * from tbl_diklat")->result();
+        $matkulnya =  $this->db->query("select * from tbl_mata_kuliah")->result();
+        $dosennya = $this->db->query("select * from tbl_dosen")->result();
+        $ruangnya =$this->db->query("select * from tbl_ruang")->result();
+        $data['diklat'] = $diklatnya;
+        $data['matkul'] = $matkulnya;
+        $data['dosen'] =  $dosennya;
+        $data['ruang'] =  $ruangnya;
+
+        $this->load->view('jadwal_kuliah/tambah',$data);
+        $this->load->view('templates_dosen/footer');
+    }
+
+    public function simpan()
+
+    {
+        $data['title'] = 'Jadwal Kuliah';
+        $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
+        $this->load->view('templates_dosen/header', $data);
+        $this->load->view('templates_dosen/sidebar_admin', $data);
+
+        $diklat = $this->input->post('id_diklat');
+        $matkul = $this->input->post('id_mata_kuliah');
+        $dosen = $this->input->post('id_dosen');
+        // $kode = $this->input->post('kode_jadwal');
+        $tgl = $this->input->post('tanggal');
+        $waktu = $this->input->post('waktu');
+        $tempat = $this->input->post('id_ruang');
+        $ket = $this->input->post('keterangan');
+
+        $data = array(
+            'id_diklat' => $diklat,
+            'id_mata_kuliah' => $matkul,
+            'id_dosen' => $dosen,
+            // 'kode_jadwal' => $kode,
+            'tanggal' => $tgl,
+            'waktu' => time(),
+            'id_ruang' => $tempat,
+            'keterangan' => $ket
+        );
+
+        $this->m_jadwal->savedata($data, 'tbl_jadwal_kuliah');
+        redirect('jadwal_kuliah');
+        $this->load->view('templates_dosen/footer');
+    }
+
+    public function edit($id){
+
+    }
+
+    public function editaksi(){
+
+
+    }
+
+    public function hapus(){
+
+        $this->db->query("delete from tbl_jadwal_kuliah where id_jadwal_kuliah ='" . $id . "'");
+
+        redirect('menu/submenu','refresh');
+    }
+}
---
+++ application/views/jadwal_kuliah/index.php
@@ -0,0 +1,67 @@
+<title><?= $title; ?></title>
+
+<div class="container-fluid">
+    <div class="card mb-4 py-1 border-left-primary">
+        <div class="card-body">
+            Jadwal Kuliah
+        </div>
+    </div>
+
+    <?php echo $this->session->flashdata('pesan') ?>
+
+    <?php echo anchor('jadwal_kuliah/tambah', '<button class="btn btn-sm btn-primary mb-3"><i class="fas fa-plus fa-sm"></i> Tambah Jadwal Kuliah</button>') ?>
+
+    <!--table -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Jadwal Kuliah</h6>
+        </div>
+
+        <div class="card-body">
+            <div class="table-responsive">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th width="10px">No</th>
+                                    <th width="50px">Nama Diklat</th>
+                                    <th width="50px">Mata Kuliah</th>
+                                    <th width="50px">Nama Dosen</th>
+                                    <!-- <th width="50px">Kode</th> -->
+                                    <th width="50px">Tanggal</th>
+                                    <th width="50px">Waktu</th>
+                                    <th width="50px">Tempat</th>
+                                    <th width="50px">Keterangan</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $no = 1;
+                                foreach ($jadwal as $jdw) : ?>
+                                    <tr>
+                                        <td width="20px"><?php echo $no++ ?></td>
+                                        <td><?= $jdw->nama_diklat ?></td>
+                                        <td><?= $jdw->nama_mata_kuliah ?></td>
+                                        <td><?= $jdw->nama ?></td>
+                                        <!-- <td><?= $jdw->kode_jadwal ?></td> -->
+                                        <td><?= $jdw->tanggal ?></td>
+                                        <td><?= $jdw->waktu ?></td>
+                                        <td><?= $jdw->nama_ruang ?></td>
+                                        <td><?= $jdw->keterangan ?></td>
+
+                                        <td width="20px"> <?php echo anchor('diklat/adminedit/' . $jdw->id_jadwal_kuliah, '<div class="btn btn-sm btn-primary"><i class="fa fa-edit"></i></div>') ?>
+                                            <?php echo anchor('jadwal_kuliah/hapus/' . $jdw->id_jadwal_kuliah, '<div class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></div>') ?></td>
+                                    </tr>
+                                <?php
+                                endforeach
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
