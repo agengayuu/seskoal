95ebdf1d22fc73ef08c93e658e18cc73e12c5f9a
Dhiya'Ulhaq Putri Kinanty <70521610+putriulhaq@users.noreply.github.com>
2022-03-11 17:07:29 +0700

    fix set pas and hak akses

--- application/controllers/Hak_akses.php
+++ application/controllers/Hak_akses.php
@@ -39,7 +39,7 @@ class Hak_akses extends CI_Controller{
         $data['user'] = $this->db->get_where('user', ['username'=> $this->session->userdata('username')])->row_array();
     
         //$data['diklatnya'] = $this->m_diklat->tampildata()->result();
-        $this->load->view('templates_dosen/header'); 
+        $this->load->view('templates_dosen/header',$data); 
         $this->load->view('templates_dosen/sidebar_admin',$data);
     
         $data['role'] = $this->db->get_where('grupuser', ['id_grup_user' => $id_role])->row_array();
