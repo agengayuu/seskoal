d215e39e7a1cf94f1c3500a45727d2a55189d564
Dhiya'Ulhaq Putri Kinanty <70521610+putriulhaq@users.noreply.github.com>
2022-03-14 10:43:55 +0700

    fix profile_dosen

--- application/controllers/Profile_dosen.php
+++ application/controllers/Profile_dosen.php
@@ -10,31 +10,27 @@ class Profile_dosen extends CI_Controller{
         $this->load->library('form_validation');
         $this->load->model('m_profil_dosen');
         $this->load->library('session');
-        if(!$this->session->userdata('username')){
-            redirect('login');
-        }
+        // if(!$this->session->userdata('username')){
+        //     redirect('login');
+        // }
        // session_start();
     }
     public function index(){
         $data['title'] = 'Profile Dosen';
-        
-        $data['user'] = $this->db->get_where('user', ['username'=> 
-        $this->session->userdata('username')])->row_array(); 
+        $data['user'] = $this->db->get_where('user', ['username' =>
+        $this->session->userdata('username')])->row_array();
+        $this->load->view('templates_dosen/header', $data);
+        $this->load->view('templates_dosen/sidebar_admin', $data); 
 
-        
         $userlogin = $this->session->userdata('username');
         $dosen = $this->db->query("SELECT tbl_dosen.*, user.*
                         FROM tbl_dosen
                         JOIN user ON tbl_dosen.nip = user.username
                         WHERE tbl_dosen.nip = $userlogin ")->result();
 
-        $data['user'] = $dosen;
-        $this->load->view('templates_dosen/header',$data);  
-        $this->load->view('templates_dosen/sidebar_admin',$data); 
-
+        $data['user'] = $dosen; 
         $data['profilnya'] = $this->db->query("select * from tbl_dosen")->result();
         
-
         $this->load->view('profile_dosen/index', $data); 
         $this->load->view('templates_dosen/footer'); 
 
