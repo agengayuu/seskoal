0f3d2d363cdc44e47b3439af8ab3515be8f7b0cb
agengayuu <ayuageng@apps.ipb.ac.id>
2022-03-16 09:01:52 +0700

    Add Frontend Hasil Evaluasi Mahasiswa in Mahasiswa

--- application/controllers/Profil_mahasiswa.php
+++ application/controllers/Profil_mahasiswa.php
@@ -11,7 +11,7 @@ class Profil_mahasiswa extends CI_Controller
         $this->load->library('form_validation');
         $this->load->model('m_profil_mahasiswa');
         $this->load->library('session');
-        is_logged_in('2');
+        is_logged_in('1');
         //session_start();
     }
 
